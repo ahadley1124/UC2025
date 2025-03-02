e8c23890543bae868d64a5ee9e5d9838acd59c91
Austin Hadley <admin@austinh.dev>
2025-03-01 21:18:03 -0500

    Both Motors Spin
    Signed-off-by: Austin Hadley <admin@austinh.dev>

--- rust/target/debug/deps/rust-d2d01d8c8994bc5f.d
+++
@@ -1,5 +0,0 @@
-/home/austin/GitHub/UC2025/rust/target/debug/deps/rust-d2d01d8c8994bc5f: src/main.rs
-
-/home/austin/GitHub/UC2025/rust/target/debug/deps/rust-d2d01d8c8994bc5f.d: src/main.rs
-
-src/main.rs:
--- src/main.py
+++ src/main.py
@@ -1,58 +1,58 @@
+# SPDX-FileCopyrightText: 2021 ladyada for Adafruit Industries
+# SPDX-License-Identifier: MIT
+# This example uses an Adafruit Stepper and DC Motor FeatherWing to run a Stepper Motor.
+#   https://www.adafruit.com/product/2927
 import time
 from board import SCL, SDA
 import busio
+# Import the PCA9685 module. Available in the bundle and here:
+#   https://github.com/adafruit/Adafruit_CircuitPython_PCA9685
 from adafruit_pca9685 import PCA9685
-from adafruit_motor import servo
-
+from adafruit_motor import stepper
 i2c = busio.I2C(SCL, SDA)
+# Create a simple PCA9685 class instance for the Motor FeatherWing's default address.
+pca = PCA9685(i2c, address=0x60)
+pca1 = PCA9685(i2c, address=0x60)
+pca.frequency = 1600
+pca1.frequency = 1600
+# Motor 1 is channels 9 and 10 with 8 held high.
+# Motor 2 is channels 11 and 12 with 13 held high.
+# Motor 3 is channels 3 and 4 with 2 held high.
+# Motor 4 is channels 5 and 6 with 7 held high.
 
-# Create a simple PCA9685 class instance.
-print("Here")
-pca = PCA9685(i2c)
+pca1.channels[8].duty_cycle = 0xFFFF
+pca1.channels[13].duty_cycle = 0xFFFF
 
-# You can optionally provide a finer tuned reference clock speed to improve the accuracy of the
-# timing pulses. This calibration will be specific to each board and its environment. See the
-# calibration.py example in the PCA9685 driver.
-# pca = PCA9685(i2c, reference_clock_speed=25630710)
+pca.channels[7].duty_cycle = 0xFFFF
 
-pca.frequency = 50
+pca.channels[2].duty_cycle = 0xFFFF
 
-# To get the full range of the servo you will likely need to adjust the min_pulse and max_pulse to
-# match the stall points of the servo.
-# This is an example for the Sub-micro servo: https://www.adafruit.com/product/2201
-# servo7 = servo.Servo(pca.channels[7], min_pulse=580, max_pulse=2350)
-# This is an example for the Micro Servo - High Powered, High Torque Metal Gear:
-#   https://www.adafruit.com/product/2307
-# servo7 = servo.Servo(pca.channels[7], min_pulse=500, max_pulse=2600)
-# This is an example for the Standard servo - TowerPro SG-5010 - 5010:
-#   https://www.adafruit.com/product/155
-# servo7 = servo.Servo(pca.channels[7], min_pulse=400, max_pulse=2400)
-# This is an example for the Analog Feedback Servo: https://www.adafruit.com/product/1404
-# servo7 = servo.Servo(pca.channels[7], min_pulse=600, max_pulse=2500)
-# This is an example for the Micro servo - TowerPro SG-92R: https://www.adafruit.com/product/169
-# servo7 = servo.Servo(pca.channels[7], min_pulse=500, max_pulse=2400)
-# The pulse range is 750 - 2250 by default. This range typically gives 135 degrees of
-# range, but the default is to use 180 degrees. You can specify the expected range if you wish:
-# servo7 = servo.Servo(pca.channels[7], actuation_range=135)
+stepper_motor_EL = stepper.StepperMotor(
+    pca.channels[4], pca.channels[3], pca.channels[5], pca.channels[6]
+)
 
-servo7 = servo.Servo(pca.channels[7])
+stepper_motor_AZ = stepper.StepperMotor(
+    pca.channels[9], pca.channels[10], pca.channels[11], pca.channels[12],
+)
 
-# We sleep in the loops to give the servo time to move into position.
-for i in range(180):
-    servo7.angle = i
-    time.sleep(0.1)
+for i in range(100):
 
-for i in range(180):
-    servo7.angle = 180 - i
-    time.sleep(0.1)
+    stepper_motor_EL.onestep()
+    stepper_motor_AZ.onestep()
 
-# You can also specify the movement fractionally.
+    time.sleep(0.01)
 
-fraction = 0.0
 
-while fraction < 1.0:
-    servo7.fraction = fraction
-    fraction += 0.01
-    time.sleep(0.03)
+for i in range(100):
+
+    stepper_motor_EL.onestep(direction=stepper.BACKWARD)
+    stepper_motor_AZ.onestep(direction=stepper.BACKWARD)
+
+    time.sleep(0.01)
+
+
+#for j in range(20):
+#	for i in range(100):
+#		stepper_motor_EL.onestep()
 
 pca.deinit()
